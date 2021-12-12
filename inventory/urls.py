5c4eb142f4e41a341accdcd43a44bf1d4d10bfd2
Marek Schir <schir2@msn.com>
2021-12-12 18:45:32 -0500

    added link to storage detail

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from inventory.views.storage_views import StorageList
+from inventory.views.storage_views import StorageList, StorageDetail
 
 urlpatterns = [
-    path('storage', include([
+    path('storage/', include([
         path('', StorageList.as_view(), name='storage_list'),
+        path('<int:pk>', StorageDetail.as_view(), name='storage_detail')
     ])),
 ]
--- inventory/views/storage_views.py
+++ inventory/views/storage_views.py
@@ -1,13 +1,27 @@
 from django.views.generic import DetailView, ListView, UpdateView, DeleteView
 
-from inventory.models.inventory_models import Storage
+from inventory.models.inventory_models import Storage, Item
 
 
 class StorageList(ListView):
     model = Storage
     template_name_suffix = '_list'
+    extra_context = {'title': 'Storage List'}
 
 
 class StorageDetail(DetailView):
     model = Storage
     template_name_suffix = '_detail'
+    extra_context = {'title': 'Storage Detail'}
+
+
+class ItemList(ListView):
+    model = Item
+    template_name_suffix = '_list'
+    extra_context = {'title': 'Item List'}
+
+
+class ItemDetail(DetailView):
+    model = Item
+    template_name_suffix = '_detail'
+    extra_context = {'title': 'Item Detail'}
