b399356ad667dd85d223f187e08897041c31f17d
Marek Schir <schir2@msn.com>
2021-12-22 00:54:15 -0500

    added basic qr code functionality

--- inventory_mgmt/settings.py
+++ inventory_mgmt/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'mptt',
+    'qr_code',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
