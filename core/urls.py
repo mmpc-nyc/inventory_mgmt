8504d1257f8406d1691edad3616e4a201383964e
Marek Schir <schir2@gmail.com>
2021-12-27 11:35:21 -0500

    renamed inventory_mgmt to core

---
+++ core/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+
+from inventory.viewsets import BrandViewSet
+from core import settings
+
+router = routers.DefaultRouter()
+router.register(r'brands', BrandViewSet)
+
+urlpatterns = [
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/', include(router.urls)),
+    path('accounts/', include('allauth.urls'), name='socialaccount_signup'),
+    path('admin/', admin.site.urls),
+    path('main/', include(('main.urls', 'main'), namespace='main')),
+    path('', include(('inventory.urls', 'inventory'), namespace='inventory')),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
