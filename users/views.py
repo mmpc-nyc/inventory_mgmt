533403bccb6122e6d19253c49ed695874395635d
Marek Schir <schir2@gmail.com>
2021-12-27 18:19:41 -0500

    added some styles

--- users/views.py
+++ users/views.py
@@ -1,18 +1,20 @@
-from django.views.generic import FormView
-from django.forms import Form
-from django.views import View
-from django.contrib.auth import logout, authenticate, login
-from rest_framework.reverse import reverse_lazy
 from django.contrib import messages
-from core import settings
-from users.forms import LoginForm
+from django.contrib.auth import logout, authenticate, login
+from django.forms import Form
 from django.http import HttpResponseRedirect
 from django.utils.translation import gettext_lazy as _
+from django.views import View
+from django.views.generic import FormView
+from django.views.generic.base import ContextMixin
+from rest_framework.reverse import reverse_lazy
+
+from users.forms import LoginForm
 
 
-class LoginView(FormView):
+class LoginView(FormView, ContextMixin):
     template_name = 'users/login_form.html'
     form_class = LoginForm
+    extra_context = {'title': 'User Login'}
 
     def post(self, request, *args, **kwargs):
 
@@ -26,8 +28,10 @@ class LoginView(FormView):
                     login(request=request, user=user)
                     messages.success(request, f'Logged in as {user.username}')
                     return self.form_valid(form)
-                messages.error(request, _(f'Login failed, Your account is inactive. Please contact an administrator to reactivate your account'))
-            messages.error(request, _(f'Failed to login. Either the user does not exist or you have entered an invalid password'))
+                messages.error(request,
+                               _(f'Login failed, Your account is inactive. Please contact an administrator to reactivate your account'))
+            messages.error(request,
+                           _(f'Failed to login. Either the user does not exist or you have entered an invalid password'))
         return self.form_invalid(form)
 
     def get_success_url(self):
@@ -35,7 +39,7 @@ class LoginView(FormView):
 
 
 class LogoutView(View):
-
     def get(self, request, *args, **kwargs):
         logout(request)
+        messages.info(request, 'User Logged Off')
         return HttpResponseRedirect(reverse_lazy('inventory:home'))
