303f37376ea16e3a3d7c4cb5b5343b340afab3ca
Marek Schir <schir2@gmail.com>
2022-01-02 00:25:33 -0500

    added viewsets and serializers for rest api

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,110 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from inventory.models import Equipment, GenericProduct, Category, Contact, Customer, Location, Email, Order, Stock
+from inventory.models.product import Brand, Product, ProductType
 
-from inventory.models.product import Brand
+User = get_user_model()
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'url', 'username', 'first_name', 'last_name', 'email', ]
+
+
+class LocationSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Location
+        fields = ['id', 'url', 'name']
+
+
+class PhoneNumberSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Location
+        fields = ['id', 'url', 'phone_number']
+
+
+class EmailSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Email
+        fields = ['id', 'url', 'email']
+
+
+class ContactSerializer(serializers.HyperlinkedModelSerializer):
+    emails = EmailSerializer(many=True, )
+    phone_numbers = PhoneNumberSerializer(many=True, )
+
+    class Meta:
+        model = Contact
+        fields = ['id', 'url', 'first_name', 'last_name', 'emails', 'phone_numbers']
+
+
+class CustomerSerializer(serializers.HyperlinkedModelSerializer):
+    contacts = ContactSerializer(many=True, )
+    locations = LocationSerializer(many=True, )
+
+    class Meta:
+        model = Customer
+        fields = ['id', 'url', 'first_name', 'last_name', 'company_name', 'contacts', 'parent', 'locations']
 
 
 class BrandSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Brand
         fields = ['id', 'url', 'name']
+
+
+class CategorySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['name', 'parent']
+
+
+class GenericProductSerializer(serializers.HyperlinkedModelSerializer):
+    category = CategorySerializer()
+
+    class Meta:
+        model = GenericProduct
+        fields = ['id', 'url', 'name', 'category', 'status']
+
+
+class ProductTypeSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = ProductType
+        fields = ['id', 'url', 'name']
+
+
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
+    brand = BrandSerializer()
+    generic_product = GenericProductSerializer()
+    product_type = ProductTypeSerializer()
+
+    class Meta:
+        model = Product
+        fields = ['id', 'url', 'name', 'brand', 'generic_product', 'status', 'product_type', ]
+
+
+class StockSerializer(serializers.HyperlinkedModelSerializer):
+    location = LocationSerializer()
+
+    class Meta:
+        model = Stock
+        fields = ['id', 'url', 'name', 'status', 'location']
+
+
+class EquipmentSerializer(serializers.HyperlinkedModelSerializer):
+    product = ProductSerializer()
+    stock = StockSerializer()
+
+    class Meta:
+        model = Equipment
+        fields = ['id', 'url', 'name', 'product', 'status', 'condition', 'stock', 'employee', ]
+
+
+class OrderSerializer(serializers.HyperlinkedModelSerializer):
+    customer = CustomerSerializer()
+    location = LocationSerializer()
+
+    class Meta:
+        model = Order
+        fields = ['id', 'url', 'customer', 'status', 'location', 'date']
--- core/urls.py
+++ core/urls.py
@@ -3,24 +3,36 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 
-from api.viewsets import BrandViewSet
+from api.viewsets import BrandViewSet, EquipmentViewSet, ProductViewSet, EmailViewSet, PhoneNumberViewSet, \
+    ContactViewSet, CustomerViewSet, GenericProductViewSet, LocationViewSet, CategoryViewSet, OrderViewSet, \
+    StockViewSet, ProductTypeViewSet, UserViewSet
 from core import settings
 from users.views import LoginView, LogoutView
 
 router = routers.DefaultRouter()
 router.register(r'brands', BrandViewSet)
+router.register(r'equipments', EquipmentViewSet)
+router.register(r'products', ProductViewSet)
+router.register(r'emails', EmailViewSet)
+router.register(r'phone_numbers', PhoneNumberViewSet)
+router.register(r'contacts', ContactViewSet)
+router.register(r'customers', CustomerViewSet)
+router.register(r'generic_products', GenericProductViewSet)
+router.register(r'locations', LocationViewSet)
+router.register(r'categories', CategoryViewSet)
+router.register(r'orders', OrderViewSet)
+router.register(r'stock', StockViewSet)
+router.register(r'product_types', ProductTypeViewSet)
+router.register(r'users', UserViewSet)
 
-urlpatterns = [
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('api/', include(router.urls)),
-    path('accounts/', include('allauth.urls'), name='socialaccount_signup'),
-    path('admin/', admin.site.urls),
-    path('main/', include(('main.urls', 'main'), namespace='main')),
-    path('', include(('inventory.urls', 'inventory'), namespace='inventory')),
-    path('users/', include(('users.urls', 'users'), namespace='users')),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-]
+urlpatterns = [path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+               path('api/', include(router.urls)),
+               path('accounts/', include('allauth.urls'), name='socialaccount_signup'), path('admin/', admin.site.urls),
+               path('main/', include(('main.urls', 'main'), namespace='main')),
+               path('', include(('inventory.urls', 'inventory'), namespace='inventory')),
+               path('users/', include(('users.urls', 'users'), namespace='users')),
+               path('login/', LoginView.as_view(), name='login'),
+               path('logout/', LogoutView.as_view(), name='logout'), ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
