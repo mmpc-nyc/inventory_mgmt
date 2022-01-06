4b7134b235d3dbc3541dfc349ed0657d0f8798af
Marek Schir <schir2@gmail.com>
2022-01-06 17:58:52 -0500

    converted equipments to map from list

--- frontend/inventory_mgmt/src/components/EquipmentListItem.vue
+++ frontend/inventory_mgmt/src/components/EquipmentListItem.vue
@@ -1,5 +1,6 @@
 <template>
   <li>
+    <span class="actions"><button @click="remove"><i class="material-icons">delete</i> </button></span>
     <span class="name">{{ equipment.name }}</span>
     <span class="category">{{ equipment.category}}</span>
     <span class="condition">{{ equipment.condition }}</span>
@@ -10,8 +11,12 @@
 </template>
 <script>
 export default {
-  props: ["equipment"],
-  methods: {}
+  props: ["equipment", "id"],
+  methods: {
+    remove(){
+      this.$store.commit("equipments/delete", this.equipment)
+    }
+  }
 
 }
 </script>
