f9e1ea22bacf201328bf8e2a5e976030b01d73fa
Marek Schir <schir2@gmail.com>
2022-01-06 17:57:53 -0500

    fixed some bugs with equpment checkin

--- frontend/inventory_mgmt/src/views/EquipmentCheckin.vue
+++ frontend/inventory_mgmt/src/views/EquipmentCheckin.vue
@@ -1,14 +1,22 @@
 <template>
   <h1>Equipment Checkin</h1>
   <the-scanner @onScanSuccess="onScanSuccess"></the-scanner>
+  <equipment-list :equipments="equipments"></equipment-list>
 </template>
 <script>
 import TheScanner from '@/components/TheScanner'
+import EquipmentList from "@/components/EquipmentList";
+
 export default {
   name: 'Equipment Checkin',
-  components: {TheScanner},
+  components: {EquipmentList, TheScanner},
+  computed: {
+    equipments() {
+      return this.$store.state.equipments.equipments
+    }
+  },
   methods: {
-    onScanSuccess(payload){
+    onScanSuccess(payload) {
       this.$store.dispatch("equipments/checkIn", payload.decodedText)
     }
   }
