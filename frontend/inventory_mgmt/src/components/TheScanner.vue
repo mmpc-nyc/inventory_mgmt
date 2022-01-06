dead21a30f5e6e2e47d3239f17209a4b06a93201
Marek Schir <schir2@gmail.com>
2022-01-06 18:00:36 -0500

    added scanner component

---
+++ frontend/inventory_mgmt/src/components/TheScanner.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="qr-scanner"></div>
+</template>
+<script>
+import {Html5QrcodeScanner} from 'html5-qrcode'
+
+export default {
+  name: "QrScanner",
+  props: {
+    qrbox: {
+      type: Number,
+      default: 250
+    },
+    fps: {
+      type: Number,
+      default: 10
+    },
+  },
+  mounted() {
+    const config = {
+      fps: this.fps,
+      qrbox: this.qrbox,
+    };
+    const qrScanner = new Html5QrcodeScanner('qr-scanner', config);
+    qrScanner.render(this.onScanSuccess);
+  },
+  methods: {
+    onScanSuccess(decodedText, decodedResult) {
+      this.$emit('onScanSuccess', {decodedText: decodedText, decodedResult: decodedResult});
+    }
+  }
+}
+</script>
