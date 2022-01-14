d1df541d169fbca953a9399ef336df27e70b131f
Marek Schir <schir2@msn.com>
2022-01-13 23:36:34 -0500

    added additional test data

--- inventory/tests/test_base.py
+++ inventory/tests/test_base.py
@@ -1,8 +1,9 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.utils import timezone
 
 from inventory.models import Location, Stock, Customer, CustomerLocation, Brand, ProductType, GenericProduct, Product, \
-    Condition, Equipment, Order
+    Condition, Equipment, Order, OrderGenericProduct, OrderEquipment
 
 User = get_user_model()
 
@@ -14,12 +15,16 @@ class AbstractTest(TestCase):
         self.user = User.objects.create(username='user', password='user')
         self.recipient = User.objects.create(username='recipient', password='recipient', )
         self.location_stock = Location.objects.create(raw='Stock Location')
-        self.location_customer = Location.objects.create(raw='Customer Location')
+        self.location_customer_1 = Location.objects.create(raw='Customer Location 1')
+        self.location_customer_2 = Location.objects.create(raw='Customer Location 2')
         self.location_user = Location.objects.create(raw='User Location')
         self.stock = Stock.objects.create(name='Stock 1', location=self.location_stock)
-        self.customer = Customer.objects.create(first_name='Test', last_name='Customer')
-        self.customer_location = CustomerLocation.objects.create(customer=self.customer,
-                                                                 location=self.location_customer)
+        self.customer_1 = Customer.objects.create(first_name='Test', last_name='Customer 1')
+        self.customer_2 = Customer.objects.create(first_name='Test', last_name='Customer 2')
+        self.customer_1_location_1 = CustomerLocation.objects.create(customer=self.customer_1,
+                                                                     location=self.location_customer_1)
+        self.customer_location_2 = CustomerLocation.objects.create(customer=self.customer_2,
+                                                                   location=self.location_customer_2)
         self.brand = Brand.objects.create(name='Brand 1')
         self.product_type_equipment = ProductType.objects.create(name='Equipment')
         self.generic_product = GenericProduct.objects.create(name='Generic Product 1')
@@ -29,10 +34,90 @@ class AbstractTest(TestCase):
         self.condition_damaged = Condition.objects.get(name='Damaged')
         self.condition_decommissioned = Condition.objects.get(name='Decommissioned')
         self.equipment_stored_working = Equipment.objects.create(name='stored_working', product=self.product,
-            condition=self.condition_working, stock=self.stock, location=self.stock.location
+                                                                 condition=self.condition_working, stock=self.stock,
+                                                                 location=self.stock.location
 
+                                                                 )
+        self.equipment_picked_up_working_1 = Equipment.objects.create(name='picked_up_working_1', product=self.product,
+                                                                      condition=self.condition_working, stock=self.stock,
+                                                                      location=self.location_user, user=self.user,
+                                                                      status=Equipment.Status.PICKED_UP)
+        self.equipment_picked_up_working_2 = Equipment.objects.create(name='picked_up_working_2', product=self.product,
+                                                                      condition=self.condition_working, stock=self.stock,
+                                                                      location=self.location_user, user=self.user,
+                                                                      status=Equipment.Status.PICKED_UP)
+        self.equipment_deployed_working_1 = Equipment.objects.create(name='deployed_working_1', product=self.product,
+                                                                     condition=self.condition_working, stock=self.stock,
+                                                                     status=Equipment.Status.DEPLOYED)
+        self.equipment_deployed_working_2 = Equipment.objects.create(name='deployed_working_2', product=self.product,
+                                                                     condition=self.condition_working, stock=self.stock,
+                                                                     status=Equipment.Status.DEPLOYED)
+
+        self.order_deploy_initial = Order.objects.create(
+            activity= Order.Activity.DEPLOY,
+            customer= self.customer_1,
+            location= self.customer_1_location_1.location,
+            date=timezone.now(),
         )
-        self.equipment_picked_up_working = Equipment.objects.create(name='picked_up_working', product=self.product,
-            condition=self.condition_working, stock=self.stock, location=self.location_user, user=self.user,
-            status=Equipment.Status.PICKED_UP)
-        self.order = Order.objects.get(id=1)
+        self.order_deploy_partial = Order.objects.create(
+            activity=Order.Activity.DEPLOY,
+            customer=self.customer_1,
+            location=self.customer_1_location_1.location,
+            date=timezone.now(),
+        )
+        self.order_deploy_complete = Order.objects.create(
+            activity=Order.Activity.DEPLOY,
+            customer=self.customer_1,
+            location=self.customer_1_location_1.location,
+            date=timezone.now(),
+        )
+        OrderGenericProduct.objects.create(
+            order=self.order_deploy_initial,
+            generic_product=self.generic_product,
+            quantity=2
+        )
+        OrderGenericProduct.objects.create(
+            order=self.order_deploy_partial,
+            generic_product=self.generic_product,
+            quantity=2
+        )
+        OrderGenericProduct.objects.create(
+            order=self.order_deploy_complete,
+            generic_product=self.generic_product,
+            quantity=2
+        )
+        OrderEquipment.objects.create(order=self.order_deploy_partial, equipment= self.equipment_deployed_working_1)
+        OrderEquipment.objects.create(order=self.order_deploy_complete, equipment= self.equipment_deployed_working_1)
+        OrderEquipment.objects.create(order=self.order_deploy_complete, equipment= self.equipment_deployed_working_2)
+
+        self.order_collect_initial = Order.objects.create(
+            activity= Order.Activity.COLLECT,
+            customer= self.customer_1,
+            location= self.customer_1_location_1.location,
+            date=timezone.now()
+        )
+        self.order_collect_partial = Order.objects.create(
+            activity= Order.Activity.COLLECT,
+            customer= self.customer_1,
+            location= self.customer_1_location_1.location,
+            date=timezone.now()
+        )
+        self.order_collect_complete = Order.objects.create(
+            activity= Order.Activity.COLLECT,
+            customer= self.customer_1,
+            location= self.customer_1_location_1.location,
+            date=timezone.now()
+        )
+        OrderEquipment.objects.create(order=self.order_collect_initial, equipment= self.equipment_deployed_working_1)
+        OrderEquipment.objects.create(order=self.order_collect_initial, equipment= self.equipment_deployed_working_2)
+        OrderEquipment.objects.create(order=self.order_collect_partial, equipment= self.equipment_picked_up_working_1)
+        OrderEquipment.objects.create(order=self.order_collect_partial, equipment= self.equipment_deployed_working_2)
+        OrderEquipment.objects.create(order=self.order_collect_complete, equipment= self.equipment_picked_up_working_1)
+        OrderEquipment.objects.create(order=self.order_collect_complete, equipment= self.equipment_picked_up_working_2)
+
+        self.order_inspect = Order.objects.create(
+            activity= Order.Activity.INSPECT,
+            customer= self.customer_1,
+            location= self.customer_1_location_1.location,
+            date=timezone.now()
+        )
