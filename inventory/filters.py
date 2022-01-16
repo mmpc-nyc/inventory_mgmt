0c2b703c5d17ca4414338eac7cdc3defbb415f09
Marek Schir <schir2@msn.com>
2022-01-16 17:21:04 -0500

    fixed imports

--- inventory/filters.py
+++ inventory/filters.py
@@ -1,20 +1,18 @@
-from django_filters import FilterSet, rest_framework as filters
-from inventory.models.modelsimport Product, Stock
 from django.contrib.auth import get_user_model
+from django_filters import FilterSet, rest_framework as filters
+
+from inventory.models.product import Product
+from inventory.models.stock import Stock
 
 User = get_user_model()
 
 
 class ProductFilter(FilterSet):
-
     name = filters.CharFilter(field_name='name', lookup_expr='contains')
-    status = filters.ChoiceFilter(choices= Product.Status.choices)
+    status = filters.ChoiceFilter(choices=Product.Status.choices)
     stock = filters.ModelChoiceFilter(queryset=Stock.objects.all())
     user = filters.ModelChoiceFilter(queryset=User.objects.all())
 
     class Meta:
         model = Product
-        fields = (
-            'name',
-            'status',
-        )
+        fields = ('name', 'status',)
