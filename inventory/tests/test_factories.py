d3966d8fd10e2aaf0293fd3fd2cd48aaba6d1d33
Marek Schir <schir2@msn.com>
2022-01-16 17:22:05 -0500

    added file for testing factories

---
+++ inventory/tests/test_factories.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+
+
+class TestOrderFactory(TestCase):
+    def test_create_from_orders(self):
+        self.fail()
+
+    def test_create_from_equipment(self):
+        self.fail()
