fdf4ac4c492f660627070fbf9309e51186eead4d
Marek Schir <schir2@msn.com>
2022-01-17 19:19:03 -0500

    set a default for user profile location

--- users/models.py
+++ users/models.py
@@ -20,7 +20,7 @@ class User(AbstractUser):
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    location = models.ForeignKey(Location, on_delete=models.CASCADE, blank=True, null=True)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return f'{self.user.username}'
