b06f309ee4091a2277b39a89e9c4523cdf24c4fd
Marek Schir <schir2@msn.com>
2022-01-17 19:16:50 -0500

    created CreateOrderForm.vue initial file

---
+++ frontend/inventory_mgmt/src/views/CreateOrderForm.vue
@@ -0,0 +1,18 @@
+<template>
+  <form @submit="submitForm"></form>
+</template>
+
+<script>
+export default {
+  name: "CreateOrderForm.vue",
+  methods: {
+    submitForm(){
+      console.log('Submit Form Pressed')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
