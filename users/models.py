feb2ab056f9ddd1177b2be1322a4fb2025dba01e
Marek Schir <schir2@msn.com>
2022-01-17 19:15:59 -0500

    added user profiile

--- users/models.py
+++ users/models.py
@@ -1,5 +1,9 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 from simple_history.models import HistoricalRecords
+
+from inventory.models.location import Location
 
 
 class User(AbstractUser):
@@ -12,3 +16,17 @@ class User(AbstractUser):
         return ' '.join(
             name for name in [self.first_name, self.last_name] if name
         ) or self.username
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.user.username}'
+
+    class Meta:
+        verbose_name = _('Profile')
+        verbose_name_plural = _('Profiles')
+
+
