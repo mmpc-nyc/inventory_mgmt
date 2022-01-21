c1ffe60ac97f60ba6ddb5fc08577270eb0297d94
Marek Schir <schir2@msn.com>
2022-01-20 19:51:32 -0500

    added main to all views

---
+++ frontend/inventory_mgmt/src/views/admin/customer/CustomerAdminCreate.vue
@@ -0,0 +1,21 @@
+<template>
+  <form @submit.prevent="createCustomer">
+    <button type="submit">Submit</button>
+  </form>
+
+</template>
+
+<script>
+export default {
+  name: "CustomerAdminCreate",
+  methods: {
+    createCustomer(){
+      console.log("Create Customer")
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
