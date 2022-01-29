fea2ebb4d9806f2d61dbbc73d7c7805437db7c0d
Marek Schir <schir2@msn.com>
2022-01-28 23:00:18 -0500

    fixed bugs related to typescript migration

--- frontend/inventory_mgmt/src/stores/UserStore.ts
+++ frontend/inventory_mgmt/src/stores/UserStore.ts
@@ -1,39 +1,41 @@
 import axios from "axios";
 import {config} from "@/config/config"
-import {Commit} from "vuex";
-import {User} from "@/models/user";
+import {ActionTree, Commit, MutationTree} from "vuex";
+import {AuthUser} from "@/models/authUser";
 
 const BASE_URL = `${config.BASE_URL}/users`
 
-export interface UserState {
-    user: User
-    users: User[]
+class State {
+    user: AuthUser | null = null
+    users: AuthUser[] = []
+}
+
+const mutations = <MutationTree<State>>{
+    getList(state: State, users: AuthUser[]) {
+        //TODO Fix This - Replace AuthUser with User
+        state.users = users
+    }
+}
+
+const actions = <ActionTree<State, any>>{
+    getList({commit}: { commit: Commit }) {
+        axios.get(BASE_URL).then(
+            response => {
+                commit('GET_ALL', response.data)
+            }
+        ).catch(
+            () => {
+                return 'Failed to connect to API'
+            }
+        )
+    }
 }
 
 const userStore = {
-    state() {
-        return {}
-    },
+    state: new State(),
     namespaced: true,
-    actions: {
-
-        getList({commit}: { commit: Commit }) {
-            axios.get(BASE_URL).then(
-                response => {
-                    commit('GET_ALL', response.data)
-                }
-            ).catch(
-                () => {
-                    return 'Failed to connect to API'
-                }
-            )
-        }
-    },
-    mutations: {
-        GET_ALL(state: UserState, users: User[]) {
-            state.users = users
-        }
-    }
+    actions: actions,
+    mutations: mutations
 }
 
 export default userStore
