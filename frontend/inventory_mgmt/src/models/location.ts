eabbcf19b618974285acd52c9ee324b499e56de7
Marek Schir <schir2@msn.com>
2022-01-29 22:05:36 -0500

    defined order and location models

--- frontend/inventory_mgmt/src/models/location.ts
+++ frontend/inventory_mgmt/src/models/location.ts
@@ -1,3 +1,14 @@
+export class LocationState{
+    name: string = ""
+}
+
 export class Location{
     id?: number
+    address_line_1: string = ""
+    address_line_2: string = ""
+    city: string = ""
+    state?: LocationState
+    postal_code: string = ""
+    latitude?: number
+    longitude?: number
 }
--- frontend/inventory_mgmt/src/models/order.ts
+++ frontend/inventory_mgmt/src/models/order.ts
@@ -7,50 +7,27 @@ import {Location} from "@/models/location";
 export type OrderActivity = "Deploy" | "Collect" | "Inspect"
 export type OrderStatus = "New" | "Assigned" | "In Progress" | "Completed" | "Canceled"
 
-export interface Order {
-    id: number
-    activity: OrderActivity
-    customer: Customer
-    status: OrderStatus
-    location: Location
-    equipments: Equipment[]
-    team_lead: User
-    team: User[]
-    generic_products: GenericProduct[]
-    date: string
+export class Order {
+    id?: number
+    activity: OrderActivity = "Deploy"
+    customer?: Customer
+    status: OrderStatus = "New"
+    location?: Location
+    equipments: Equipment[] = []
+    team_lead?: User
+    team: User[] = []
+    generic_products: GenericProduct[] = []
+    date: string = ""
 }
 
-class DeployOrder implements Order {
-    id: number;
+export class DeployOrder extends Order {
     activity: OrderActivity = "Deploy"
-    status: OrderStatus;
-    customer: Customer;
-    location: Location;
-    equipments: Equipment[];
-    generic_products: GenericProduct[];
-    team_lead: User;
-    team: User[];
-    date: string;
+}
 
-    constructor(
-        id: number,
-        customer: number | Customer,
-        status: OrderStatus,
-        team_lead: User,
-        team: User[],
-        location: Location,
-        date: string,
-        equipments: Equipment[],
-        generic_products: GenericProduct[]
-    ) {
-        this.id = id
-        this.status = status
-        this.customer = customer
-        this.location = location
-        this.equipments = equipments
-        this.generic_products = generic_products
-        this.team_lead = team_lead
-        this.team = team
-        this.date = date
-    }
+export class CollectOrder extends Order {
+    activity: OrderActivity = "Collect"
+}
+
+export class InspectOrder extends Order {
+    activity: OrderActivity = "Inspect"
 }
