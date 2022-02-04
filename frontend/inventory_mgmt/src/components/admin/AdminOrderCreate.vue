97796579ac33a5404f687d46f450e7eed71fb70b
Marek Schir <schir2@gmail.com>
2022-02-04 18:10:25 -0500

    added select location functionality to create order

--- frontend/inventory_mgmt/src/components/admin/AdminOrderCreate.vue
+++ frontend/inventory_mgmt/src/components/admin/AdminOrderCreate.vue
@@ -1,5 +1,22 @@
 <template>
   <ui-card v-if="newOrder">
+    <section class="date">
+      <h2>Date</h2>
+      <ui-datepicker
+          v-model="newOrder.date"
+          :config="config"
+          placeholder="Select Datetime.."
+          toggle
+          clear
+      >
+        <template #toggle>
+          <i class="fa fa-calendar"></i>
+        </template>
+        <template #clear>
+          <i class="fa fa-close"></i>
+        </template>
+      </ui-datepicker>
+    </section>
     <section class="type">
       <h2>Order Activity</h2>
       <ui-select v-model="newOrder.activity" :options="newOrder.getOrderActivities()"></ui-select>
@@ -20,18 +37,35 @@
                                           class="customer-details">
       </admin-order-create-customer-detail>
     </section>
-    <section v-if="newOrder.customer" class="location">
-      <h2>Location</h2>
-      <ui-autocomplete
-          :source="newOrder.customer.service_locations"
-          :source-format="{label: 'name', value:'id'}"
-          fullwidth
-      >
-      </ui-autocomplete>
+    <section v-if="newOrder.customer">
+      <h2>Service Location {{ newOrder.location ? newOrder.location.name : "" }}</h2>
+      <admin-order-create-location-selection
+          :locations="newOrder.customer.service_locations"
+          @onSelectLocation="onSelectLocation"
+      ></admin-order-create-location-selection>
     </section>
-    <section class="generic-products"><h2>Generic Products</h2></section>
+    <section v-if="newOrder.activity === 'Deploy'" class="generic-products">
+      <h2>Generic Products</h2>
+    <ui-autocomplete
+          v-model="genericProductSearchText"
+          delay="500"
+          @search="onSearchGenericProduct"
+          @selected="onSelectGenericProduct"
+          :source="genericProductSearchResults"
+          :source-format="{ label: 'name', value: 'id'}"
+          remote
+          fullwidth></ui-autocomplete>
+    </section>
     <section class="equipments"><h2>Equipment</h2></section>
-    <section class="date"><h2>Date</h2></section>
+    <ui-autocomplete
+          v-model="equipmentSearchText"
+          delay="500"
+          @search="onSearchEquipment"
+          @selected="onSelectEquipment"
+          :source="equipmentSearchResults"
+          :source-format="{ label: 'name', value: 'id'}"
+          remote
+          fullwidth></ui-autocomplete>
     <section class="team"><h2>Team</h2></section>
   </ui-card>
 
@@ -41,16 +75,27 @@
 
 import customerService from "@/services/CustomerService.ts";
 import AdminOrderCreateCustomerDetail from "@/components/admin/AdminOrderCreateCustomerDetail";
+import AdminOrderCreateLocationSelection from "./AdminOrderCreateLocationSelection";
+import genericProductService from "../../services/GenericProductService";
+import equipmentService from "../../services/EquipmentService";
 
 export default {
   name: "AdminOrderCreate",
-  components: {AdminOrderCreateCustomerDetail},
+  components: {AdminOrderCreateLocationSelection, AdminOrderCreateCustomerDetail},
   props: ["order"],
   data() {
     return {
       newOrder: {},
       customerSearchText: "",
       customerSearchResults: [],
+      equipmentSearchText: "",
+      equipmentSearchResults: [],
+      genericProductSearchText: "",
+      genericProductSearchResults: [],
+      config: {
+        enableTime: true,
+        dateFormat: 'Y-m-d H:i'
+      },
     }
   },
   created() {
@@ -62,8 +107,24 @@ export default {
     },
     onSelectCustomer(customer) {
       this.newOrder.customer = customer
-    }
-
+    },
+    onSelectLocation(location) {
+      this.newOrder.location = location
+    },
+    onSelectEquipment(equipment){
+      console.log(equipment)
+    },
+    async onSearchEquipment(text) {
+      this.equipmentSearchResults = await equipmentService.search(text)
+      console.log(text)
+    },
+    onSelectGenericProduct(genericProduct){
+      console.log(genericProduct)
+    },
+    async onSearchGenericProduct(text) {
+      this.genericProductSearchResults = await genericProductService.search(text)
+      console.log(text)
+    },
   },
   watch: {
     newOrder: {
---
+++ frontend/inventory_mgmt/src/components/admin/AdminOrderCreateLocationSelectionItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <ui-item @click="onSelectLocation" :class="{selected:location.selected }">
+    {{ location.address_line_1 }}
+  </ui-item>
+</template>
+
+<script>
+export default {
+  name: "AdminOrderCreateLocationSelectionItem",
+  props: ["location"],
+  methods: {
+    onSelectLocation() {
+      this.$emit("onSelectLocation", this.location)
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
