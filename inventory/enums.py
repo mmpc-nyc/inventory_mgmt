b8f4198430c42998556acc597c22eb83c9f87072
Marek Schir <schir2@gmail.com>
2023-02-16 16:33:55 -0500

    added vendor

--- inventory/enums.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.utils.translation import gettext_lazy as _
-
-
-class OrderActivity(models.TextChoices):
-    DEPLOY = 'Deploy', _('Deploy')
-    COLLECT = 'Collect', _('Collect')
-    INSPECT = 'Inspect', _('Inspect')
---
+++ inventory/models/vendor.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
+
+
+class Vendor(models.Model):
+    """A person, company, or organization that sells goods."""
+    name = models.CharField(max_length=150, blank=True)
+    active = models.BooleanField(default=True)
+    delivery_time = models.PositiveIntegerField()
+    website = models.URLField(max_length=256, default='', blank=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = _('Vendor')
+        verbose_name_plural = _('Vendor')
+
+    def get_absolute_url(self):
+        return reverse_lazy('warehouse:vendor_detail', kwargs={'pk': self.pk})
+
+
+class VendorProduct(models.Model):
+    """good or service that is produced or supplied by an external vendor or supplier,
+    rather than being produced in-house. It is offered for sale by the vendor."""
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    sku = models.CharField(max_length=256)
+
