b0d2cb7b9ff320dbdae91769eef294babf39de14
Marek Schir <schir2@msn.com>
2023-03-22 13:57:11 -0400

    added inventory imports

---
+++ inventory/models/__init__.py
@@ -0,0 +1,6 @@
+from .brand import Brand
+from .equipment import Equipment, EquipmentCategory, EquipmentField, EquipmentItem, EquipmentClass
+from .material import Material, MaterialCategory, MaterialClass, MaterialClassMembership
+from .stock_location import StockLocation
+from .transfer import Transfer, TransferAcceptance
+from .vendor import Vendor, VendorMaterial
