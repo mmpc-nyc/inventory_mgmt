686ceb06ea1a882f903f965b837989fea4278561
Marek Schir <schir2@msn.com>
2023-04-05 20:47:21 -0400

    added orders models

---
+++ orders/models/__init__.py
@@ -0,0 +1,6 @@
+from .job import Job, JobProduct, JobService, JobMaterial
+from .schedule import Schedule
+from .service import Service, ServiceProduct, ServiceWarranty, ServiceMaterialClass, SuggestedServiceMaterial, \
+    RequiredServiceMaterial, RequiredServiceEquipment, PricingScheme
+from .service_package import ServicePackage, ServicePackageItem
+from .warranty import Warranty
